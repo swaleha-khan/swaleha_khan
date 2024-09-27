2371c4274f31ccb635cf49b6cae5b2529cbdbcb8
swalehakh <swalehaa1600khan@gmail.com>
2024-09-27 16:12:07 +0530

    first commit

--- program1.py
+++ program1.py
@@ -1,20 +1,24 @@
+import unittest
+
 class Solution(object):
     def isValid(self, s):
         """
         :type s: str
         :rtype: bool
         """
-        pass
+        stack = []
+        bracket_map = {')': '(', '}': '{', ']': '['}
 
+        for char in s:
+            if char in bracket_map.values():
+                stack.append(char)
+            elif char in bracket_map.keys():
+                if not stack or stack.pop() != bracket_map[char]:
+                    return False
 
-
-
-
-
-
+        return not stack
     
-
-
-
-  
-
+solution = Solution()
+print(solution.isValid("()"))  
+print(solution.isValid("()[]{}"))  
+print(solution.isValid("{[()]}"))  
--- program2.py
+++ program2.py
@@ -1,10 +1,22 @@
+import unittest
+
 class Solution(object):
     def romanToInt(self, s):
         """
         :type s: str
         :rtype: int
         """
-        pass
+        if not s:  # Check for empty string
+            return 0
+        
+        roman_map = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        result = 0
+        
+        for i in range(len(s) - 1):
+            if roman_map[s[i]] < roman_map[s[i + 1]]:
+                result -= roman_map[s[i]]  # Subtract if current is less than next
+            else:
+                result += roman_map[s[i]]  # Add if current is greater or equal
 
-
-
+        result += roman_map[s[-1]]  # Add the last character's value
+        return result
